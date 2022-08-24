de11ecfe3fb3f3dfda6c4b18cf1455a9b3a7ebdd
MarcinKorcz101 <korekasiorek@gmail.com>
2022-08-24 20:26:16 +0200

    Finished bottom navbar + edit of templates

--- AM_144446_145276/app/src/main/java/com/example/am_144446_145276/MainActivity.kt
+++ AM_144446_145276/app/src/main/java/com/example/am_144446_145276/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.am_144446_145276
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import com.example.am_144446_145276.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(MainFragment())
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId) {
+                R.id.meetings -> replaceFragment(MainFragment())
+                R.id.add_meeting -> replaceFragment(AddMeetingFragment())
+                R.id.profile -> replaceFragment(UserProfileFragment())
+                else -> {
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
+        fragmentTransaction.commit()
     }
 }
