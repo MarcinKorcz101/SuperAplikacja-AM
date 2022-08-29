693a5c42385d878bc72dbff367ebcd8dfec4e5ad
MarcinKorcz101 <korekasiorek@gmail.com>
2022-08-29 19:49:48 +0200

    Updated bottom navbar

--- AM_144446_145276/app/src/main/java/com/example/am_144446_145276/MainActivity.kt
+++ AM_144446_145276/app/src/main/java/com/example/am_144446_145276/MainActivity.kt
@@ -3,34 +3,26 @@ package com.example.am_144446_145276
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.am_144446_145276.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        replaceFragment(MainFragment())
-        binding.bottomNavigationView.setOnItemSelectedListener {
+        setContentView(R.layout.activity_main)
 
-            when(it.itemId) {
-                R.id.meetings -> replaceFragment(MainFragment())
-                R.id.add_meeting -> replaceFragment(AddMeetingFragment())
-                R.id.profile -> replaceFragment(UserProfileFragment())
-                else -> {
-                }
-            }
-            true
-        }
-    }
-
-    private fun replaceFragment(fragment: Fragment){
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
-        fragmentTransaction.commit()
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.mainFragment, R.id.joinMeetingFragment, R.id.userProfileFragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+//
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
