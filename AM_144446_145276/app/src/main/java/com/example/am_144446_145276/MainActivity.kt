eb5805782d1363b743ee0db5d989cf23f138d4b2
Akaczek <luk12349876@o2.pl>
2022-09-16 20:32:02 +0200

    Basic login activity

--- AM_144446_145276/app/src/main/java/com/example/am_144446_145276/MainActivity.kt
+++ AM_144446_145276/app/src/main/java/com/example/am_144446_145276/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.am_144446_145276.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
