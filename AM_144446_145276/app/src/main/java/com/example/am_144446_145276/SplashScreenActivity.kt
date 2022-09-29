ee5b6d19e7046681f8f8470565dc62a54c6ef9b7
Akaczek <luk12349876@o2.pl>
2022-09-29 21:17:56 +0200

    Add animation on start

--- AM_144446_145276/app/src/main/java/com/example/am_144446_145276/SplashScreenActivity.kt
+++ AM_144446_145276/app/src/main/java/com/example/am_144446_145276/SplashScreenActivity.kt
@@ -1,11 +1,18 @@
 package com.example.am_144446_145276
 
+import android.animation.Animator
+import android.animation.Animator.AnimatorListener
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
 
 @SuppressLint("Cus", "CustomSplashScreen")
 @Suppress("DEPRECATION")
@@ -20,12 +27,41 @@ class SplashScreenActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        // we used the postDelayed(Runnable, time) method
-        // to send a message with a delayed time.
-        Handler().postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 1500)
+        fun startAnimation(context: Context) {
+            val splashImage = findViewById<ImageView>(R.id.splashScreenImage)
+
+            val animationSet = AnimatorSet()
+
+            val scaleY = ObjectAnimator.ofFloat(splashImage, "scaleY", 35f, 1f).setDuration(2000)
+            val scaleX = ObjectAnimator.ofFloat(splashImage, "scaleX", 35f, 1f).setDuration(2000)
+
+            animationSet.playTogether(scaleX, scaleY)
+            animationSet.start()
+            animationSet.addListener(object : AnimatorListener {
+                override fun onAnimationStart(animation: Animator) {
+                    // ...
+                }
+
+                override fun onAnimationRepeat(animation: Animator) {
+                    // ...
+                }
+
+                override fun onAnimationEnd(animation: Animator) {
+                    Handler().postDelayed({
+                        val intent = Intent(context, LoginActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }, 1000)
+                }
+
+                override fun onAnimationCancel(animation: Animator) {
+                    // ...
+                }
+            })
+        }
+
+        startAnimation(this)
+
+
     }
 }
